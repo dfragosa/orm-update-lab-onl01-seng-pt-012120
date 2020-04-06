a412953e7189ba47392e695650f51caf0ba854a5
Derric Fragosa <fderric@gmail.com>
2020-04-06 21:09:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,9 @@ end
 
 def self.create_table
   sql = <<-SQL
-  CREATE TABLE students
+  CREATE TABLE students (id INTEGER PRIMARY KEY,
+  name TEXT grade TEXT);
   SQL
+  DB[:conn].execute(sql)
 
 end

4fbd77718486b75dbacc5a3819a97989a418ba2e
Derric Fragosa <fderric@gmail.com>
2020-04-06 21:22:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ def save
     VALUES (?,?)
   SQL
   DB[:conn].execute(sql, self.name, self.grade)
-  @id = DB[:conn].execute("SELECT last_insert_rowid()")
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
 end
 
 end

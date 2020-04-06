e4e00f7202f39aed8e8f440e228c26939569fcf0
Derric Fragosa <fderric@gmail.com>
2020-04-06 22:24:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,11 @@ def self.find_by_name(name)
   sql = <<-SQL
   SELECT * FROM students
   WHERE name = ?
+  LIMIT 1
   SQL
+  DB[:conn].execute.map do |row|
+    self.new_from_db(row)
+  end
 end
 
 def update
